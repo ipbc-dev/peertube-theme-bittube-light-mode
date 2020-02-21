84496a8a93832a5f4a2fc1f3065dce88cd14b2e3
ICabaleiro <i.cabaleiro@devcups.com>
2020-02-21 16:49:22 +0000

    Adding 404 image and styles

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -4,8 +4,10 @@ function register ({ registerHook, peertubeHelpers }) {
   const filterUrl = baseStaticUrl + '/images/filter_black.png';
   const keyboardUrl = baseStaticUrl + '/images/keyboard_black.png';
   const languagesUrl = baseStaticUrl + '/images/languages_black.png';
+  const image404 = baseStaticUrl + '/images/404.svg'
   const style = document.createElement('style')
   document.head.appendChild(style)
+  // STYLES START
   const sheet = style.sheet
       // Change filter icon
       let rule = "#custom-css .icon.icon-filter { "
@@ -22,7 +24,22 @@ function register ({ registerHook, peertubeHelpers }) {
       rule+= " background-image: url('"+ keyboardUrl +"'); -webkit-filter: none; filter: none"
       rule+= " } "
       sheet.insertRule(rule, 0)
-  // END
+  // STYLES END
+
+  /* ANGULAR NAVIGATION-END */
+    registerHook({
+      target: 'action:router.navigation-end',
+      handler: params => {
+        console.warn("======= INJECTING 404 ====== path is:", params.path)
+        if(document.querySelector('my-page-not-found')){
+          const img = document.querySelector('my-page-not-found').querySelector('img')
+          if(img){
+            console.log("REACHING THE POINT!!!!!!")
+            img.src = image404
+          }
+        }
+      }
+    })
 }
 
 export {
