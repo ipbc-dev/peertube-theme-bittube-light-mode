9643e92256ff8296b4bef1ff034c96ac12e3ad3b
ICabaleiro <i.cabaleiro@devcups.com>
2020-02-21 16:51:35 +0000

    Cleaning

--- client/common-client-plugin.js
+++ client/common-client-plugin.js
@@ -30,11 +30,9 @@ function register ({ registerHook, peertubeHelpers }) {
     registerHook({
       target: 'action:router.navigation-end',
       handler: params => {
-        console.warn("======= INJECTING 404 ====== path is:", params.path)
         if(document.querySelector('my-page-not-found')){
           const img = document.querySelector('my-page-not-found').querySelector('img')
           if(img){
-            console.log("REACHING THE POINT!!!!!!")
             img.src = image404
           }
         }
